04724b3b24014db0f6d1611face5d529a44b0bbc
SadidSD <SadidSD@users.noreply.github.com>
2026-06-18 22:55:15 +0600

    style: remove lower ribbon from contact page hero logo

--- app/contact/LargeLogo.js
+++ app/contact/LargeLogo.js
@@ -11,19 +11,229 @@ export default function LargeLogo({ className }) {
         justifyContent: "center",
         alignItems: "center",
         position: "relative",
+        width: "100%",
+        height: "100%",
       }}
     >
-      {/* New Aeethod "A" mark logo */}
-      <img
-        src="/logo-icon.png"
-        alt="Aeethod Logo"
-        style={{
-          width: "70%",
-          height: "70%",
-          objectFit: "contain",
-          filter: "drop-shadow(0 8px 16px rgba(0,0,0,0.12))",
-        }}
-      />
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="-25 -25 270 320"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ overflow: "visible", display: "block" }}
+      >
+        <defs>
+          {/* Shared neumorphic filter for ribbon shapes */}
+          <filter
+            id="hero_filter_ribbon"
+            x="-20"
+            y="-20"
+            width="250"
+            height="200"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="-1" dy="-1" />
+            <feGaussianBlur stdDeviation="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.5 0"
+            />
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_ribbon" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="1" dy="1" />
+            <feGaussianBlur stdDeviation="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.768627 0 0 0 0 0.890196 0 0 0 0 1 0 0 0 0.3 0"
+            />
+            <feBlend mode="normal" in2="effect1_dropShadow_ribbon" result="effect2_dropShadow_ribbon" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_ribbon" result="shape" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="5" dy="5" />
+            <feGaussianBlur stdDeviation="6.5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.9 0"
+            />
+            <feBlend mode="normal" in2="shape" result="effect3_innerShadow_ribbon" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="-5" dy="-5" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.768627 0 0 0 0 0.890196 0 0 0 0 1 0 0 0 0.9 0"
+            />
+            <feBlend mode="normal" in2="effect3_innerShadow_ribbon" result="effect4_innerShadow_ribbon" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="5" dy="-5" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.2 0"
+            />
+            <feBlend mode="normal" in2="effect4_innerShadow_ribbon" result="effect5_innerShadow_ribbon" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="-5" dy="5" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.2 0"
+            />
+            <feBlend mode="normal" in2="effect5_innerShadow_ribbon" result="effect6_innerShadow_ribbon" />
+          </filter>
+
+          {/* Shared neumorphic filter for blue triangle body */}
+          <filter
+            id="hero_filter_body"
+            x="-20"
+            y="-20"
+            width="300"
+            height="350"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="-1" dy="-1" />
+            <feGaussianBlur stdDeviation="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.5 0"
+            />
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_body" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="1" dy="1" />
+            <feGaussianBlur stdDeviation="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.768627 0 0 0 0 0.890196 0 0 0 0 1 0 0 0 0.3 0"
+            />
+            <feBlend mode="normal" in2="effect1_dropShadow_body" result="effect2_dropShadow_body" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_body" result="shape" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="5" dy="5" />
+            <feGaussianBlur stdDeviation="6.5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.9 0"
+            />
+            <feBlend mode="normal" in2="shape" result="effect3_innerShadow_body" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="-5" dy="-5" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.768627 0 0 0 0 0.890196 0 0 0 0 1 0 0 0 0.9 0"
+            />
+            <feBlend mode="normal" in2="effect3_innerShadow_body" result="effect4_innerShadow_body" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="5" dy="-5" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.2 0"
+            />
+            <feBlend mode="normal" in2="effect4_innerShadow_body" result="effect5_innerShadow_body" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dx="-5" dy="5" />
+            <feGaussianBlur stdDeviation="5" />
+            <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0.627451 0 0 0 0 0.72549 0 0 0 0 0.898039 0 0 0 0.2 0"
+            />
+            <feBlend mode="normal" in2="effect5_innerShadow_body" result="effect6_innerShadow_body" />
+          </filter>
+        </defs>
+
+        {/* Vector 11 — Blue triangle body (#B2CEFE) */}
+        <g filter="url(#hero_filter_body)">
+          <path
+            d="M79.6073 3L15.678 132.197C15.678 132.197 -5.1784 170.088 6.61001 152.74C18.3984 135.392 44.0415 115.424 61.4713 119.871C69.3946 121.892 81.3439 130.586 85.5015 141.784C90.4889 144.523 120.609 212.419 133.109 233.545C133.109 233.545 156.585 269.196 212 264.589L79.6073 3Z"
+            fill="#B2CEFE"
+          />
+        </g>
+
+
+        {/* Vector 10 — Upper white filled ribbon (top "beak" of the A) */}
+        <g filter="url(#hero_filter_ribbon)">
+          <path
+            d="M39.4444 29.9034L3 95C3 95 44.1852 63.0364 52.7778 58.3588C61.3704 53.6812 68.4864 47.5195 80.3333 43.9362C94.8183 39.555 104.346 38.9197 119.444 41.2076C138.419 44.0828 152.926 55.1105 163 61.4772C163 59.5282 147 42.377 147 42.377C142.556 36.9198 117.667 11.9726 103.444 6.5154C89.2222 1.0582 78.1111 3.0072 67.8888 5.346C59.711 7.21704 45.5185 22.4972 39.4444 29.9034Z"
+            fill="#FFFFFF"
+          />
+        </g>
+      </svg>
     </div>
   );
 }
